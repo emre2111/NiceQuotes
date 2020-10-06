ba62b859b90a1fcc4e79e9df34644dd1493327de
emre <yilmaz.ey@gmail.com>
2020-10-06 23:33:59 +0200

    add first app

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 // Import-Anweisungen
 import React, { Component } from "react";
-import { Button, StyleSheet, View } from "react-native";
+import { Button, StyleSheet, View, Platform, SafeAreaView } from "react-native";
 // APIs und Komponenten
 // eigene Komponenten
 import Quote from "./js/components/Quote";
+import NewQuote from "./js/components/NewQuote";
 
 const data = [
   {
@@ -21,7 +22,18 @@ const data = [
 
 // Deklaration einer Komponente (hier als Klasse)
 export default class App extends Component {
-  state = { index: 0 }; // Initialer Zustand
+  state = { index: 0, showNewQuoteScreen: false, quotes: data }; // Initialer Zustand
+
+  _addQuote = (text, author) => {
+    // aktuelle Liste der Zitate einer Variablen zu
+    let { quotes } = this.state;
+    // Füge neue Zitate an das Ende der Liste an
+    if (text && author) {
+      quotes.push({ text, author });
+    }
+    // Aktualisiere Liste der Zitate im State
+    this.setState({ showNewQuoteScreen: false, quotes: quotes });
+  };
 
   // render: Darstellung der Komponente im UI
   // render wird automatisch ausgeführt:
@@ -29,18 +41,30 @@ export default class App extends Component {
   // b) Zustand änder sich (state) ==> this.setState(...)
   // c) wenn props sich ändern
   render() {
-    let index = this.state.index;
-    const quote = data[index];
+    let { index, quotes } = this.state;
+    const quote = quotes[index];
     let nextIndex = index + 1;
-    if (nextIndex === data.length) nextIndex = 0;
+    if (nextIndex === quotes.length) nextIndex = 0;
     return (
-      <View style={styles.container}>
-        <Quote text={quote.text} author={quote.author} />
-        <Button
-          title="Nächstes Zitat"
-          onPress={() => this.setState({ index: nextIndex })}
+      <SafeAreaView style={styles.container}>
+        <View style={styles.newButton}>
+          <Button
+            title="+"
+            onPress={() => this.setState({ showNewQuoteScreen: true })}
+          />
+        </View>
+        <NewQuote
+          visible={this.state.showNewQuoteScreen}
+          onSave={this._addQuote}
         />
-      </View>
+        <Quote text={quote.text} author={quote.author} />
+        <View style={styles.nextButton}>
+          <Button
+            title="Nächstes Zitat"
+            onPress={() => this.setState({ index: nextIndex })}
+          />
+        </View>
+      </SafeAreaView>
     );
   }
 }
@@ -49,8 +73,18 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#eee",
     alignItems: "center",
     justifyContent: "center",
+  },
+  nextButton: {
+    position: "absolute",
+    bottom: 10,
+    bottom: Platform.OS === "ios" ? 20 : 0,
+  },
+  newButton: {
+    position: "absolute",
+    right: 10,
+    top: 10,
   },
 });
--- js/components/Quote.js
+++ js/components/Quote.js
@@ -1,29 +1,40 @@
-import React, { Component, Fragment } from "react";
-import { Text } from "react-native";
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default class Quote extends Component {
-  render() {
-    const { text, author } = this.props; // destructuring
-    // const text = this.props.text
-    // const author = this.props.author
-    return (
-      <Fragment>
-        <Text>{text}</Text>
-        <Text>{author}</Text>
-      </Fragment>
-    );
-  }
-}
-
-// als Funktion geschrieben
-/*
 export default function Quote(props) {
-    const { text, author} = props;
-    return (
-        <Fragment>
-            <Text>{text}</Text>
-            <Text>-- {author}</Text>
-        </Fragment>
-    );
+  const { text, author } = props; // destructuring
+  // const text = this.props.text
+  // const author = this.props.author
+  return (
+    <View style={styles.component}>
+      <Text style={styles.text}>{text}</Text>
+      <Text style={styles.author}>&mdash; {author}</Text>
+    </View>
+  );
 }
-*/
+
+// Styling in React Native mit JavaScript
+const styles = StyleSheet.create({
+  component: {
+    paddingHorizontal: 20,
+    backgroundColor: "white",
+    margin: 10,
+    borderRadius: 4,
+    elevation: 2,
+    shadowOpacity: 0.25,
+    shadowOffset: {
+      width: 0,
+      height: 0.75,
+    },
+    shadowRadius: 1.5,
+  },
+  text: {
+    fontSize: 36,
+    fontStyle: "italic",
+    marginBottom: 10,
+  },
+  author: {
+    fontSize: 20,
+    textAlign: "right",
+  },
+});
